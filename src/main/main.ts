7ce5a374d1718c1b14f983b847af34e98b5d8fdd
Acidiney Dias <acidineydias@gmail.com>
2022-04-07 14:41:20 +0100

    fix: lint problems

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,61 +1,60 @@
 import 'reflect-metadata'
 import { config } from 'dotenv'
 
+import chalk from 'chalk'
+import { NestFactory } from '@nestjs/core'
+import { INestApplication, Logger } from '@nestjs/common'
+import expressBasicAuth from 'express-basic-auth'
+
+import { Env } from './config/env'
+import { MainModule } from '../framework/main.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+
 config()
 
-import chalk from 'chalk';
-import { NestFactory } from '@nestjs/core';
-import { INestApplication, Logger } from '@nestjs/common';
-import expressBasicAuth from 'express-basic-auth';
+function setupSwagger (app: INestApplication): void {
+  const APP_NAME = Env.appName
+  const APP_DESCRIPTION = Env.appDescription
 
-import {Env} from './config/env'
-import { MainModule } from '../framework/main.module'
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
-
-
-function setupSwagger(app: INestApplication) {
-  const APP_NAME = Env.appName;
-  const APP_DESCRIPTION = Env.appDescription;
-
-  const SWAGGER_ADMIN_USERNAME = Env.swagger.username;
-  const SWAGGER_ADMIN_PASSWORD = Env.swagger.password;
+  const SWAGGER_ADMIN_USERNAME = Env.swagger.username
+  const SWAGGER_ADMIN_PASSWORD = Env.swagger.password
 
   const options = new DocumentBuilder()
     .setTitle(APP_NAME)
     .setDescription(APP_DESCRIPTION)
     .setVersion('v1')
-    .build();
+    .build()
 
-  const document = SwaggerModule.createDocument(app, options);
+  const document = SwaggerModule.createDocument(app, options)
 
   if (Env.isProd) {
     app.use(
       '/',
       expressBasicAuth({
         users: {
-          [SWAGGER_ADMIN_USERNAME]: SWAGGER_ADMIN_PASSWORD,
+          [SWAGGER_ADMIN_USERNAME]: SWAGGER_ADMIN_PASSWORD
         },
-        challenge: true,
-      }),
-    );
+        challenge: true
+      })
+    )
   }
 
-  SwaggerModule.setup('/', app, document);
-  SwaggerModule.setup('/docs', app, document);
+  SwaggerModule.setup('/', app, document)
+  SwaggerModule.setup('/docs', app, document)
 
-  Logger.log('Mapped {/, GET} Swagger api route', 'RouterExplorer');
-  Logger.log('Mapped {/docs, GET} Swagger api route', 'RouterExplorer');
+  Logger.log('Mapped {/, GET} Swagger api route', 'RouterExplorer')
+  Logger.log('Mapped {/docs, GET} Swagger api route', 'RouterExplorer')
 }
 
-async function bootstrap() {
+async function bootstrap (): Promise<void> {
   try {
     const app: INestApplication = await NestFactory.create(MainModule, {
-      cors: true,
-    });
+      cors: true
+    })
 
     setupSwagger(app)
 
-    await app.listen(Env.port);
+    await app.listen(Env.port)
 
     if (Env.isProd) {
       Logger.log(
@@ -63,20 +62,20 @@ async function bootstrap() {
           .hex('#87e8de')
           .bold(`${Env.port}`)}`,
         'Bootstrap',
-        false,
-      );
-       return
+        false
+      )
+      return
     }
     Logger.log(
-      `🚀  Server ready at http://${Env.host}:${chalk
+      `🚀  Server ready at https://${Env.host}:${chalk
         .hex('#87e8de')
         .bold(`${Env.port}`)}`,
       'Bootstrap',
-      false,
+      false
     )
   } catch (error) {
-    Logger.error(error, 'Bootstrap');
+    Logger.error(error, 'Bootstrap')
   }
 }
 
-void bootstrap();
+void bootstrap()
